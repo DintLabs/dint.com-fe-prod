7a2165fe18d452d209cd2054366bf89654731873
Max <info@dint.com>
2023-02-24 03:00:43 -0500

    Ntifications

--- src/frontend/pages/Notifications/index.tsx
+++ src/frontend/pages/Notifications/index.tsx
@@ -1,13 +1,15 @@
 import { Box, Divider, Stack, Typography, useMediaQuery } from "@mui/material";
 import { AppDispatch, RootState } from "frontend/redux/store";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import _axios from "frontend/api/axios";
 import { fetchAllChatsList } from "frontend/redux/slices/messages";
 import { Avatar } from "@mui/material";
+import { ThemeContext } from "../../contexts/ThemeContext";
 
 const NotificationsContainer = () => {
   const [ notifications , setNotifications ] = useState([]);
+  const { toggle } = useContext(ThemeContext);
   let userId: any = JSON.parse(localStorage.getItem("userData"));
   const dispatch: AppDispatch = useDispatch();
 
@@ -22,21 +24,22 @@ const NotificationsContainer = () => {
   return (
     <Stack sx={useMediaQuery("(min-width:900px)") ? { margin: "2% 0%" } : {marginTop:"5%"}}>
 
-      <Typography variant="h2" component="h2">
+      <Typography variant="h2" component="h2"
+      style={{color: toggle ? "white" :"#666666"}}>
         Notifications
       </Typography>
       <Divider />
       <Box  sx={{margin:"2%"}}>
         <Box  sx={{margin:"2% 0%"}}>
-          <Typography variant="h4">Latest</Typography>
+          <Typography variant="h4"  style={{color: toggle ? "white" :"#666666"}}>Latest</Typography>
           <Divider />
           <Box>
             {notifications.length > 0 ? notifications.map((notif) => {
-              return (<Box sx={{display:"flex" , margin:"1% 0%" , alignItems:"center" , }}>
+              return (<Box sx={{display:"flex" , margin:"1% 0%" , alignItems:"center" ,  color: toggle ? "white" : "black", }}>
               <Avatar/>
-              <Typography sx={{margin:"0% 1%"}}>{notif.sender?.display_name} has send you a message</Typography>
+              <Typography sx={{margin:"0% 1%",  color: toggle ? "white" : "black",}}>{notif.sender?.display_name} has send you a message</Typography>
             </Box>)
-            }) : <Typography sx={{margin:"auto",textAlign:"center" , padding:"5%"}}>You have no new notifications</Typography>}
+            }) : <Typography sx={{margin:"auto",textAlign:"center" , padding:"5%",  color: toggle ? "white" : "black",}}>You have no new notifications</Typography>}
           </Box>
         </Box>
       </Box>
